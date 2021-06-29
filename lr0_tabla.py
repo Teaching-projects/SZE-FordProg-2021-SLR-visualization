22f34171f3c30d0ca1e6f4262358ee1ffc25754f
Héjja Ferenc <hejjaferenc@gmail.com>
2021-06-29 19:34:39 +0200

    teszt nyelvtan

--- lr0_tabla.py
+++ lr0_tabla.py
@@ -1,1 +1,16 @@
 # lr0 tabla
+
+
+
+# Beolvassuk a lr0 adatait tartalmazó file-t
+f = open("c:\SZE-FordProg-2021-SLR-visualization\lr0_auto.txt", "r")
+full_file=f.read()
+print(full_file)
+eol_pos=full_file.find('\n')
+print(eol_pos)
+connections=full_file[:eol_pos]
+nodes=full_file[eol_pos:]
+
+print(connections)
+print("+++++++++++++++++++++")
+print(nodes)
